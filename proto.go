d38d561433d15a015459a09799ea7db4b17f5916
zimulala <zimu_xia@126.com>
2014-09-21 23:04:19 +0800

    modify the func of ReadFromConn and UnmarshalPkg

--- proto.go
+++ proto.go
@@ -7,9 +7,12 @@ import (
 	"net"
 	"strconv"
 	"strings"
+	"time"
 )
 
 const (
+	LineNoStr   = "*"
+	DataLenStr  = "$"
 	ArgSplitStr = "\r\n"
 )
 
@@ -28,18 +31,17 @@ type Task struct {
 	Buf     []byte
 }
 
-//TODO:
 func UnmarshalPkg(pkg *Task) (key []byte, err error) {
 	elements := strings.SplitN(string(pkg.Buf), ArgSplitStr, -1)
-	if !strings.Contains(elements[0], "*") {
+	if !strings.Contains(elements[0], LineNoStr) {
 		return key, ErrBadCmdFormat
 	}
-	lines, err := strconv.Atoi(elements[0][len("*"):])
+	lines, err := strconv.Atoi(elements[0][len(LineNoStr):])
 	if err != nil || lines != 3 {
 		return key, ErrBadCmdFormat
 	}
 
-	l, err := strconv.Atoi(elements[3])
+	l, err := strconv.Atoi(strings.Trim(elements[3], DataLenStr))
 	if err != nil || l != len(elements[4]) {
 		return key, ErrBadCmdFormat
 	}
@@ -63,24 +65,24 @@ func PackErrorReply(res *Task, msg string) {
 	return
 }
 
-//TODO:
 func ReadFromConn(c *net.TCPConn) (pkg *Task, err error) {
+	c.SetReadDeadline(time.Now().Add(ConnReadDeadline * time.Second))
 	reader := bufio.NewReader(net.Conn(c))
 	dataStr, err := reader.ReadString('\n')
 	if err != nil {
 		return
 	}
 
-	if !strings.HasPrefix(dataStr, "*") {
+	if !strings.HasPrefix(dataStr, LineNoStr) {
 		return nil, ErrBadCmdFormat
 	}
-	linesStr := strings.Trim(strings.Trim(dataStr, "*"), "\r")
+	linesStr := strings.Trim(strings.Trim(dataStr, LineNoStr), ArgSplitStr)
 	lines, err := strconv.Atoi(linesStr)
 	if err != nil {
 		return
 	}
 	data := []byte(dataStr)
-	for i := 0; i < lines; i++ {
+	for i := 0; i < lines*2; i++ {
 		buf, err := reader.ReadBytes('\n')
 		if err != nil {
 			return nil, err
