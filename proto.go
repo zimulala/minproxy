bc79854312a19ab8a8a5368a2f5896e630ddb1d3
zimulala <zimu_xia@126.com>
2014-10-30 21:14:58 +0800

    add getMKeys func and modify ReadReply func

--- proto.go
+++ proto.go
@@ -30,66 +30,96 @@ var (
 	OpError uint8 = 0xFF
 )
 
+type UnitPkg struct {
+	badConn bool
+	conn    *net.TCPConn
+	uId     int
+	key     []byte
+	data    []byte
+}
+
 type Task struct {
-	Opcode    uint8
-	Id        int64
-	OutReader *bufio.Reader
-	OutConn   *net.TCPConn
-	Buf       []byte
+	Opcode   uint8
+	Id       int64
+	OutInfos []*UnitPkg
+	Buf      []byte
 }
 
-/*
-*3\r\n
-$4\r\n
-HSET\r\n
-$6\r\n
-myhash\r\n
-$5\r\n
-field1\r\n
-$3\r\n
-foo\r\n
-*/
-func UnmarshalPkg(pkg *Task) (key []byte, err error) {
-	elements := bytes.SplitN(pkg.Buf, ArgSplitBytes, -1)
-	if !bytes.HasPrefix(elements[0], LineNumBytes) {
-		return key, ErrBadCmdFormat
-	}
-
-	var lenBuf []byte
-	rows, err := strconv.Atoi(string(Trims(elements[0], LineNumStr, ArgSplitStr)))
-	if err != nil {
-		return
-	}
-	switch rows <= 1 {
-	case true:
-		return nil, ErrBadArgsNum
-	case false:
-		if len(elements) < 5 {
-			return nil, ErrBadArgsNum
-		}
-		lenBuf = elements[3]
-		key = elements[4]
-	}
-
-	argLen, err := strconv.Atoi(string(bytes.Trim(lenBuf, DataLenStr)))
-	if err != nil || argLen != len(key) {
-		return nil, ErrBadCmdFormat
-	}
-
-	return
-}
-
-func IsErrTask(task *Task) (err bool) {
-	if task.Opcode == OpError {
+func (t *Task) IsErrTask() (err bool) {
+	if t.Opcode == OpError {
 		err = true
 	}
 
 	return
 }
 
-func PackErrorReply(res *Task, msg string) {
-	res.Opcode = OpError
-	res.Buf = []byte("-" + msg + "\r\n")
+func (t *Task) PackErrorReply(msg string) {
+	t.Opcode = OpError
+	t.Buf = []byte("-" + msg + "\r\n")
+
+	return
+}
+
+func getMKeys(e [][]byte, pkg *Task) {
+	interval := 2
+	val := ArgSplitBytes
+	if string(e[2]) == "mset" {
+		interval = 4
+	}
+
+	begin := 3
+	for i := begin; i < len(e); i += interval {
+		if interval == 4 {
+			val = append(bytes.Join([][]byte{e[i+2], e[i+3]}, ArgSplitBytes), ArgSplitBytes...)
+		}
+
+		info := &UnitPkg{uId: i - begin, key: e[i+1],
+			data: bytes.Join([][]byte{[]byte("*3"), e[1], e[2], e[i], e[i+1], val}, ArgSplitBytes)}
+		pkg.OutInfos = append(pkg.OutInfos, info)
+	}
+
+	return
+}
+
+/*
+*4\r\n
+$4\r\n
+HSET\r\n
+$6\r\n
+myhash\r\n
+$5\r\n
+field1\r\n
+$0\r\n
+\r\n
+*/
+func UnmarshalPkg(pkg *Task) (err error) {
+	elements := bytes.SplitN(pkg.Buf, ArgSplitBytes, -1)
+	if !bytes.HasPrefix(elements[0], LineNumBytes) {
+		return ErrBadCmdFormat
+	}
+
+	lineN, err := strconv.Atoi(string(Trims(elements[0], LineNumStr, ArgSplitStr)))
+	if err != nil {
+		return
+	}
+	switch lineN <= 1 {
+	case true:
+		return ErrBadArgsNum
+	case false:
+		if len(elements) < 5 {
+			return ErrBadArgsNum
+		}
+		if string(elements[2]) == "mset" || string(elements[2]) == "mget" {
+			getMKeys(elements, pkg)
+			break
+		}
+
+		pkg.OutInfos = append(pkg.OutInfos, &UnitPkg{uId: 0, key: elements[4], data: pkg.Buf})
+		// argLen, err := strconv.Atoi(string(bytes.Trim(elements[3], DataLenStr)))
+		// if err != nil || argLen != len(key) {
+		// 	return ErrBadCmdFormat
+		// }
+	}
 
 	return
 }
@@ -150,28 +180,28 @@ func Write(c *net.TCPConn, buf []byte) (err error) {
 	return
 }
 
-func ReadReply(pkg *Task) (err error) {
-	pkg.OutConn.SetReadDeadline(time.Now().Add(ConnReadDeadline * time.Second))
-	reader := bufio.NewReader(net.Conn(pkg.OutConn))
-	if pkg.Buf, err = reader.ReadBytes('\n'); err != nil {
+func ReadReply(pkg *UnitPkg) (err error) {
+	pkg.conn.SetReadDeadline(time.Now().Add(ConnReadDeadline * time.Second))
+	reader := bufio.NewReader(net.Conn(pkg.conn))
+	if pkg.data, err = reader.ReadBytes('\n'); err != nil {
 		return
 	}
-	if !bytes.HasPrefix(pkg.Buf, LineNumBytes) && !bytes.HasPrefix(pkg.Buf, DataLenBytes) {
+	if !bytes.HasPrefix(pkg.data, LineNumBytes) && !bytes.HasPrefix(pkg.data, DataLenBytes) {
 		return
 	}
 
-	if bytes.HasPrefix(pkg.Buf, DataLenBytes) {
-		if bufL, err := strconv.Atoi(string(Trims(pkg.Buf, DataLenStr, ArgSplitStr))); err != nil || bufL < 0 {
+	if bytes.HasPrefix(pkg.data, DataLenBytes) {
+		if bufL, err := strconv.Atoi(string(Trims(pkg.data, DataLenStr, ArgSplitStr))); err != nil || bufL < 0 {
 			return err
 		}
 
 		buf, err := reader.ReadBytes('\n')
 		if err == nil {
-			pkg.Buf = append(pkg.Buf, buf...)
+			pkg.data = append(pkg.data, buf...)
 		}
 
 		return err
 	}
 
-	return readMutilLinesData(reader, &pkg.Buf)
+	return readMutilLinesData(reader, &pkg.data)
 }
